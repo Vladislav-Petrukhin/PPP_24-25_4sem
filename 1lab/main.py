f2f1568ab444da28bcf8a36d5f28ab51ba82f1ce
L33T_VVP <146530084+Vladislav-Petrukhin@users.noreply.github.com>
2025-04-07 12:26:45 +0300

    Лабораторная работа №1

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,8 +1,36 @@
+print("DEBUG: main.py запустился!")
+
+import sys
+import argparse
+
+from server.server import Server
+from client.client import ClientApp
+
 
 def main():
-    # Ваш код здесь
-    pass
+    """
+    Точка входа в программу.
+    Можно вызывать:
+        python .\main.py --mode server
+    или
+        python .\main.py --mode client
+    """
+
+    parser = argparse.ArgumentParser(description="File Manager (Server/Client)")
+    parser.add_argument("--mode", choices=["server", "client"], required=True,
+                        help="Run in server mode or client mode.")
+
+    args = parser.parse_args()
+
+    if args.mode == "server":
+        # Запускаем сервер
+        server = Server(host='127.0.0.1', port=9090)
+        server.start()
+    elif args.mode == "client":
+        # Запускаем клиент
+        client = ClientApp(host='127.0.0.1', port=9090)
+        client.run()
+
 
 if __name__ == "__main__":
     main()
-
