73386c67d674a86ee7bdeda919720971b075cd00
L33T_VVP <146530084+Vladislav-Petrukhin@users.noreply.github.com>
2025-06-04 16:20:03 +0300

    Delete 3lab/app directory

--- 3lab/app/api/users.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Depends
-from app.schemas.user import UserRead
-from app.api.deps import get_current_user
-
-router = APIRouter(tags=["users"])
-
-@router.get("/users/me/", response_model=UserRead)
-def read_me(current_user = Depends(get_current_user)):
-    return current_user
